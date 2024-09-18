5408138ffbdfc85ca7555217b15939f32a8df627
friedyakisoba <142198791+friedyakisoba@users.noreply.github.com>
2024-09-17 22:09:26 -1000

    finished assignment 5

--- Assignment-05/a05.c
+++ Assignment-05/a05.c
@@ -1,17 +1,42 @@
-// 
-// 
-// 
+//
+//
+//
 
 #include <stdio.h>
 
 #include "getdouble.h"
 
-int main () 
+// Conversion function
+double birthdayToDays(int day, int month, int year)
+{
+
+    // Given function to convert birthdates to days
+    double days = (year * 365.25) + ((month - 1) * (365.25 / 12.0)) + day;
+
+    // Returns the total number of day
+    return days;
+}
+
+int main(void)
 {
     // Variable declarations
-    int months;
-    int day;
-    int year;
+    int birth_month;
+    int birth_day;
+    int birth_year;
+
+    // Constants for the current date 
+    #define CURRENT_DAY 17
+    #define CURRENT_MONTH 9
+    #define CURRENT_YEAR 2024
+
+    // Using the 'birthdayToDays' function calculate teh total days of the current date
+    double current_days = birthdayToDays(CURRENT_DAY, CURRENT_MONTH, CURRENT_YEAR);
+
+    // Calculate the total days since year 1 using 'birthdayToDays' function
+    double birth_days = birthdayToDays(birth_day, birth_month, birth_year);
+
+    // Calculate the age in days by subtracting the birth days from the current days
+    double age_in_days = current_days - birth_days;
 
     // Program prompt
     printf("This program will calculate your age in days.\n");
@@ -19,26 +44,24 @@ int main ()
     // Prompt that ask for the birthday month
     printf("Enter the month you were born: ");
     // Stores the input in the 'months' variable
-    months = getdouble();
+    birth_month = getdouble();
 
     // Prompt that ask for the birthday month
     printf("Enter the day you were born: ");
     // Stores the input in the 'day' variable
-    day = getdouble();
+    birth_day = getdouble();
 
     // Prompt that ask for the birthday month
     printf("Enter the year you were born: ");
     // Stores the input in the 'year' variable
-    year = getdouble();
+    birth_year = getdouble();
 
+   
+    // Prompts the current date
+    printf("Today's date is: %d/%d/%d\n", CURRENT_MONTH, CURRENT_DAY, CURRENT_YEAR);
 
-    // variable checking area 
-    printf("month = %d\n", months);
-    printf("days = %d\n", day);
-    printf("days = %d\n", year);
+    // Results of the calculation
+    printf("Your age in days is: %.0f\n", age_in_days);
 
-
-
-    
     return 0;
 }
