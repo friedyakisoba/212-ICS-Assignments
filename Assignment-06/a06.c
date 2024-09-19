5f9117a90ebb94cc0a454ee0427b7f08a724ffa7
friedyakisoba <142198791+friedyakisoba@users.noreply.github.com>
2024-09-19 02:14:50 -1000

    Started the user prompts on Assignment 06 as well as adding the necessary makefile.

---
+++ Assignment-06/a06.c
@@ -0,0 +1,15 @@
+
+// Header file for input/output
+#include <stdio.h>
+
+int main (void)
+{
+    printf("This program will display the Japanese word for a number of your choice.\n");
+
+    printf ("Enter a number from 0 to 9: ");
+    // int choice = getchar();
+
+    return 0;
+}
+
+
