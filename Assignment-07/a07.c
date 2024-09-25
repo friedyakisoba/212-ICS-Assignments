9304fda4e6eddee5b3ac275f4aa9bbd4c0b94e62
friedyakisoba <142198791+friedyakisoba@users.noreply.github.com>
2024-09-25 07:56:53 -1000

    finished 07

--- Assignment-07/a07.c
+++ Assignment-07/a07.c
@@ -1,31 +1,45 @@
 #include <stdio.h>
 
-void printJapanese (int num) {
-  const char *ones[] = {"","ichi","ni","san","yon","go","roku","nana","hachi", "kyuu"};
-  const char *tens[] = {"","jyuu", "nijyuu", "sanjyuu", "yonjyuu", "gojyuu", "rokujyuu", "nanajyuu","hachijyuu","kyuujyuu"};
-  const char *hundreds[] = {"","hyaku", "nihyaku", "sanbyaku", "yonhyaku", "gohyaku", "roppyaku", "nanahyaku", "happyaku", "kuuhyaku"};
-  const char *thousands[] = {"","sen", "nisen", "sanzen","yonsen", "rokusen","nanasen","hassen", "kyuusen"};
+void printJapanese(int num)
+{
+  const char *ones[] = {"", "ichi", "ni", "san", "yon", "go", "roku", "nana", "hachi", "kyuu"};
+  const char *tens[] = {"", "jyuu", "nijyuu", "sanjyuu", "yonjyuu", "gojyuu", "rokujyuu", "nanajyuu", "hachijyuu", "kyuujyuu"};
+  const char *hundreds[] = {"", "hyaku", "nihyaku", "sanbyaku", "yonhyaku", "gohyaku", "roppyaku", "nanahyaku", "happyaku", "kuuhyaku"};
+  const char *thousands[] = {"", "sen", "nisen", "sanzen", "yonsen", "rokusen", "nanasen", "hassen", "kyuusen"};
 
   int one = num % 10;
+  int ten = (num / 10) % 10;
+  int hundred = (num / 100) % 10;
+  int thousand = (num / 1000) % 10;
 
-  printf("%d\n", num);
+  if (thousand > 0)
+  {
+    printf("%s ", thousands[thousand]);
+  }
+  if (hundred > 0)
+  {
+    printf("%s ", hundreds[hundred]);
+  }
+  if (ten > 0)
+  {
+    printf("%s ", tens[ten]);
+  }
+  if (one > 0)
+  {
+    printf("%s ", ones[one]);
+  }
+  printf("\n");
 }
-
 
 int main(void)
 {
 
-
-
-
   printf("This program will count in Japanese from 1 to 9999.\n");
 
+   for (int i = 1; i <= 9999; i++)
+   {
+     printJapanese(i);
+   }
   
-
-    //for (int i = 0; i <= 9999; i++)
-    //{
-    //    printf("Hell World!\n");
-    //}
-    
-    return 0;
+  return 0;
 }
