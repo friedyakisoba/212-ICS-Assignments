ba761409c05add69342b40ea9afd00d13d68b123
friedyakisoba <142198791+friedyakisoba@users.noreply.github.com>
2024-09-24 19:03:07 -1000

    local save

--- Assignment-07/a07.c
+++ Assignment-07/a07.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+void printJapanese (int num) {
+  const char *ones[] = {"","ichi","ni","san","yon","go","roku","nana","hachi", "kyuu"};
+  const char *tens[] = {"","jyuu", "nijyuu", "sanjyuu", "yonjyuu", "gojyuu", "rokujyuu", "nanajyuu","hachijyuu","kyuujyuu"};
+  const char *hundreds[] = {"","hyaku", "nihyaku", "sanbyaku", "yonhyaku", "gohyaku", "roppyaku", "nanahyaku", "happyaku", "kuuhyaku"};
+  const char *thousands[] = {"","sen", "nisen", "sanzen","yonsen", "rokusen","nanasen","hassen", "kyuusen"};
+
+  int one = num % 10;
+
+  printf("%d\n", num);
+}
+
+
+int main(void)
 {
-    for (int i = 0; i <= 100; i++)
-    {
-        printf("Hell World!\n");
-    }
+
+
+
+
+  printf("This program will count in Japanese from 1 to 9999.\n");
+
+  
+
+    //for (int i = 0; i <= 9999; i++)
+    //{
+    //    printf("Hell World!\n");
+    //}
     
     return 0;
 }
